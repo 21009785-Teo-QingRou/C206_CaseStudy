4a0ec25ffe211a76a8d68f81d16131369d196464
21017859-Ethan-Yap-Cheng-Xun <106360188+21017859-Ethan-Yap-Cheng-Xun@users.noreply.github.com>
2022-08-04 16:52:44 +0800

    Update Currency.java
    Added in code so I can start doing

--- C206_CaseStudy/src/Currency.java
+++ C206_CaseStudy/src/Currency.java
@@ -1,4 +1,52 @@
 
 public class Currency {
 
+	// field
+	public String iso_Code;
+	public String name;
+	public double buy_Rate;
+	public double sell_Rate;
+	
+	// constructor
+	public Currency(String iso_Code, String name, double buy_Rate, double sell_Rate) {
+		super();
+		this.iso_Code = iso_Code;
+		this.name = name;
+		this.buy_Rate = buy_Rate;
+		this.sell_Rate = sell_Rate;
+	}
+
+	//Getter & Setter method
+	public String getIso_Code() {
+		return iso_Code;
+	}
+
+	public void setIso_Code(String iso_Code) {
+		this.iso_Code = iso_Code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBuy_Rate() {
+		return buy_Rate;
+	}
+
+	public void setBuy_Rate(double buy_Rate) {
+		this.buy_Rate = buy_Rate;
+	}
+
+	public double getSell_Rate() {
+		return sell_Rate;
+	}
+
+	public void setSell_Rate(double sell_Rate) {
+		this.sell_Rate = sell_Rate;
+	}
+
 }
